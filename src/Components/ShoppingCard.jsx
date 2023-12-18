864c0af5b8e38dac2403619f981dfc25b9ed6e67
ramanboff <bayramofframan@gmail.com>
2023-12-18 18:39:57 +0400

    Shopping-cards style updated

--- src/Components/ShoppingCard.jsx
+++ src/Components/ShoppingCard.jsx
@@ -1,8 +1,8 @@
 const ShoppingCard = ({ product }) => {
   return (
-    <div className=" bg-gray-200">
-      <img src={product.image} alt="product" className="h-[200px] w-full"  />
-      <div className="space-y-2 p-2">
+    <div className=" bg-white rounded-md  shadow-2xl">
+      <img src={product.image} alt="product" className="h-[200px] w-full object-cover rounded-t-md" />
+      <div className="space-y-2 p-2 text-gray-800">
         <h4 className="bg-green-400 px-2 rounded w-fit">{product.name}</h4>
         <h5 className="bg-yellow-300 px-2 wonded w-fit">$ {product.price}</h5>
         <button className="bg-purple-400 rounded px-2">Add to card</button>
--- src/Screens/HomeScreen.jsx
+++ src/Screens/HomeScreen.jsx
@@ -2,7 +2,7 @@ import ShoppingCard from "../Components/ShoppingCard.jsx";
 import data from "../data.jsx";
 const HomeScreen = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 mt-[20px]">
       {data.map((product) => (
         <ShoppingCard key={product.id} product={product} />
       ))}
