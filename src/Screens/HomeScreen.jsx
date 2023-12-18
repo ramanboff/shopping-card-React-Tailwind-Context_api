72a103d97f2036201fe0418ecb57852a4916d27b
ramanboff <bayramofframan@gmail.com>
2023-12-18 23:42:42 +0400

    ShoppingCard container padding top and bottom

--- src/Screens/HomeScreen.jsx
+++ src/Screens/HomeScreen.jsx
@@ -2,7 +2,7 @@ import ShoppingCard from "../Components/ShoppingCard.jsx";
 import data from "../data.jsx";
 const HomeScreen = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 p-4">
       {data.map((product) => (
         <ShoppingCard key={product.id} product={product} />
       ))}
