81e0a3c5956427522ff7c8fc7ab533b68daa3cf0
ramanboff <bayramofframan@gmail.com>
2023-12-18 18:26:53 +0400

    ShoppingCard component  and style created

--- src/Components/ShoppingCard.jsx
+++ src/Components/ShoppingCard.jsx
@@ -1,9 +1,14 @@
-const ShoppingCard = ({product}) => {
-  return ( 
-    <div>
-      <img src={product.image} alt="product" />
+const ShoppingCard = ({ product }) => {
+  return (
+    <div className=" bg-gray-200">
+      <img src={product.image} alt="product" className="h-[200px] w-full"  />
+      <div className="space-y-2 p-2">
+        <h4 className="bg-green-400 px-2 rounded w-fit">{product.name}</h4>
+        <h5 className="bg-yellow-300 px-2 wonded w-fit">$ {product.price}</h5>
+        <button className="bg-purple-400 rounded px-2">Add to card</button>
+      </div>
     </div>
-   );
-}
- 
+  );
+};
+
 export default ShoppingCard;
