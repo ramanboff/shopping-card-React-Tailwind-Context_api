9ddd738fc3a49975a34e2736f9878e89b7ebf04b
ramanboff <bayramofframan@gmail.com>
2023-12-18 23:41:04 +0400

    RAting functionallity

--- src/Components/Rating.jsx
+++ src/Components/Rating.jsx
@@ -1,29 +1,32 @@
+import { v4 as uuidv4 } from "uuid";
 
-import {v4 as uuidv4} from "uuid"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStarHalfAlt,faStar } from '@fortawesome/free-solid-svg-icons';
-const Rating = ({value,text, color}) => {
-  return ( 
+const Rating = ({ value, text, color }) => {
+  return (
     <div>
-{[1,2,3,4,5].map((rate)=> (
-  <span key={uuidv4}>
-    <i style={{color}}>
-      {value+1 === rate +0.5? <FontAwesomeIcon icon={faStarHalfAlt} />: value>=rate?<FontAwesomeIcon icon={faStar} />:<FontAwesomeIcon icon={faStar} />}
-    </i>
-  </span>
-))}
+      {[1, 2, 3, 4, 5].map((rate) => (
+        <span key={uuidv4}>
+          <i
+            style={{ color }}
+            className={
+              value + 1 === rate + 0.5
+                ? "fas fa-star-half-alt"
+                : value >= rate
+                ? "fas fa-star"
+                : "far fa-star"
+            }
+          ></i>
 
-<span>{text&&text}</span>
+          <i></i>
+        </span>
+      ))}
+
+      <span>{text && text}</span>
     </div>
-   );
-}
-
-
+  );
+};
 
 Rating.defaultProps = {
-  color:"#FFA41C"
+  color: "#FFA41C",
+};
 
-}
-
-export default Rating
- 
+export default Rating;
