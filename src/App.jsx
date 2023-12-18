1ab3cf87404bc3fd0fca873c24e61c5e126ac965
ramanboff <bayramofframan@gmail.com>
2023-12-18 09:38:12 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Container from "./Components/Container";
+import HomeScreen from "./screens/HomeScreen";
+
+function App() {
+  return (
+    <Container>
+      <div className="">
+        <HomeScreen/>
+      </div>
+    </Container>
+  );
+}
+
+export default App;
