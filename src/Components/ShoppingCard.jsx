a5d86424ab216d9d5b229b7074f59adbbed84eb5
ramanboff <bayramofframan@gmail.com>
2023-12-18 22:15:27 +0400

    props to Rating component

--- src/Components/ShoppingCard.jsx
+++ src/Components/ShoppingCard.jsx
@@ -1,11 +1,16 @@
+import Rating from "./Rating";
+
 const ShoppingCard = ({ product }) => {
   return (
     <div className=" bg-white rounded-md  shadow-2xl">
-      <img src={product.image} alt="product" className="h-[200px] w-full object-cover rounded-t-md" />
-      <div className="space-y-2 p-2 text-gray-800">
-        <h4 className="bg-green-400 px-2 rounded w-fit">{product.name}</h4>
-        <h5 className="bg-yellow-300 px-2 wonded w-fit">$ {product.price}</h5>
-        <button className="bg-purple-400 rounded px-2 hover:text-white hover:bg-gray-700 duration-200">Add to card</button>
+      <img src={product.image} alt="product" className="h-[170px] w-full object-cover rounded-t-md" />
+      <div className="space-y-1 p-2 text-gray-800">
+        <h4 className="">{product.name}</h4>
+        <h5 className="">$ {product.price}</h5>
+        <div>
+          <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+        </div>
+        <button className="bg-orange-400 rounded-full px-2 hover:text-white duration-200">Add to card</button>
       </div>
     </div>
   );
