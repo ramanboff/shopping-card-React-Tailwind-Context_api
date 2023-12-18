6e4177087df670f72c024a70ab35a994e6dfdf14
ramanboff <bayramofframan@gmail.com>
2023-12-18 23:41:58 +0400

    image size in ShoppingCard for breackpoints

--- src/Components/ShoppingCard.jsx
+++ src/Components/ShoppingCard.jsx
@@ -3,7 +3,7 @@ import Rating from "./Rating";
 const ShoppingCard = ({ product }) => {
   return (
     <div className=" bg-white rounded-md  shadow-2xl">
-      <img src={product.image} alt="product" className="h-[170px] w-full object-cover rounded-t-md" />
+      <img src={product.image} alt="product" className="h-[300px] w-full object-cover rounded-t-md sm:h-[170px]"  /> 
       <div className="space-y-1 p-2 text-gray-800">
         <h4 className="">{product.name}</h4>
         <h5 className="">$ {product.price}</h5>
